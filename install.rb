4f58939332319cfa19d4f0505d83c12f69323f9b
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2010-03-10 21:15:16 +0100

    use File.cp

--- install.rb
+++ install.rb
@@ -4,7 +4,7 @@ puts ''
 puts "  ** Copying highlight.css to #{RAILS_ROOT}/public/stylesheets/ **"
 puts ''
 
-File.copy(
+File.cp(
   File.join(File.dirname(__FILE__), 'assets', 'stylesheets', 'highlight.css'),
   File.join(RAILS_ROOT, 'public', 'stylesheets')
 )
