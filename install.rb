6624f77e4a0062010a752ed2a099fe666e6d9f96
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2010-03-10 21:19:04 +0100

    expand the path

--- install.rb
+++ install.rb
@@ -5,6 +5,6 @@ puts "  ** Copying highlight.css to #{RAILS_ROOT}/public/stylesheets/ **"
 puts ''
 
 File.cp(
-  File.join(File.dirname(__FILE__), 'assets', 'stylesheets', 'highlight.css'),
+  File.join(File.expand_path(File.dirname(__FILE__)), 'assets', 'stylesheets', 'highlight.css'),
   File.join(RAILS_ROOT, 'public', 'stylesheets')
 )
