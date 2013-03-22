86ea4e566aaba10bc50f024ef75efe9f54fdc1aa
Marco Otte-Witte <mottewitte@rim.com>
2013-03-22 17:50:15 +0100

    removed unnecessary stuff

--- spec/boot.rb
+++ spec/boot.rb
@@ -3,10 +3,4 @@ plugin_root = File.join(File.dirname(__FILE__), '..')
 require 'activesupport'
 require 'initializer'
 
-RAILS_ROOT = File.expand_path(File.dirname(__FILE__) + '/../')
-Rails::Initializer.run(:set_load_path)
-Rails::Initializer.run(:set_autoload_paths)
-
 require File.join(File.dirname(__FILE__), '/../rails/init.rb')
-
-FileUtils.mkdir_p(File.join(File.dirname(__FILE__), 'log'))
